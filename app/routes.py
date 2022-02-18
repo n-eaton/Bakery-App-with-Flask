a7a43eda62339f1d601747d25f4304edc0f12764
n-eaton <natallia.eaton@gmail.com>
2022-02-18 16:31:06 -0600

    Initial commit

---
+++ app/routes.py
@@ -0,0 +1,18 @@
+from app import app
+from flask import render_template
+
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+    
+
+@app.route('/about')
+def about():
+    return  render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
